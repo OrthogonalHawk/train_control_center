301a4deb6eaaa0f6248911aad091db523c55aa70
OrthogonalHawk <orthogonalhawk@gmail.com>
2015-02-16 03:38:52 +0000

    Modified the configuration utility method into a small class.

--- source/Configuration.py
+++ source/Configuration.py
@@ -1,29 +1,49 @@
 
-def parseConfigurationFile(file_path):
-    """Parse the provided configuration file and return a dictionary with the results."""
-    EXPECTED_PARTS_PER_LINE = 2
-    ret = {}
+class Configuration:
 
-    configFile = open(file_path, 'rb')
+    optionNotFoundString = "ERROR - Unable to provide configuration option %s. No value found in file %s"
 
-    if (configFile):
+    # Expected options
+    backgroundImageOption = "BackgroundImage"
+
+    def __init__(self, configFilePath):
+        """Class constructor; parses the provided configuration file."""
+        self.configFilePath = configFilePath
+        self.configurationOptions = self.parseConfigurationFile(self.configFilePath)
+
+    def _getConfigurationOption(self, optionName):
+        if (optionName in self.configurationOptions.keys()):
+            return self.configurationOptions[optionName]
+        else:
+            print self.optionNofFoundString % (optionName, self.configFilePath)
+            return ""
+
+    def parseConfigurationFile(self, configFilePath):
+        """Parse the provided configuration file and return a dictionary with the results."""
+        EXPECTED_PARTS_PER_LINE = 2
+        ret = {}
+
+        configFile = open(configFilePath, 'rb')
+
+        if (configFile):
     
-        # read through the configuration file, looking for important values
-        for line in configFile:
+            # read through the configuration file, looking for important values
+            for line in configFile:
 
-            # remove any trailing newline characters
-            curLine = line.strip()
+                # remove any trailing newline characters
+                curLine = line.strip()
 
-            # split the current line on any comma ',' characters
-            parts = curLine.split(',')
+                # split the current line on any comma ',' characters
+                parts = curLine.split(',')
 
-            if (len(parts) == EXPECTED_PARTS_PER_LINE):
-                ret[parts[0]] = parts[1]
+                if (len(parts) == EXPECTED_PARTS_PER_LINE):
+                    ret[parts[0]] = parts[1]
 
-    else:
-        print "ERROR - Unable to open the provided configuration file: (%s)" % (file_path)
+        else:
+            print "ERROR - Unable to open the provided configuration file: (%s)" % (file_path)
 
-    print ret
+        return ret
 
-    return ret
+    def getBackgroundImagePath(self):
+        return self._getConfigurationOption(self.backgroundImageOption)
 
