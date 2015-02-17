c1e81261b48a531afd3d1b5af7cf0f990916372a
OrthogonalHawk <orthogonalhawk@gmail.com>
2015-02-17 03:00:37 +0000

    Several new features:   1) Parsing command line arguments   2) Reading a configuration file to obtain the desired background image   3) Implemented a sleep to prevent CPU monopolization

--- TrainControlCenter.py
+++ TrainControlCenter.py
@@ -1,33 +1,60 @@
 #!/usr/bin/python
 #!/bin/env python
 
-# import the pygame module, so you can use it
 import pygame
+import source.Configuration
+import sys
+import time
+
+window_size = (480, 272)
+
+def printUsage():
+    print "%s <configurationFilePath>" % (sys.argv[0])
 
 # define a main function
 def main():
     
+    # check the number of input arguments
+    if (len(sys.argv) != 2):
+        printUsage()
+        sys.exit(-1)
+
     # initialize the pygame module
     pygame.init()
 
-    # set the window caption
-    pygame.display.set_caption("minimal program")
+    # read the provided configuration file
+    config = source.Configuration.Configuration(sys.argv[1])
+
+    # create the display screen; set FULL SCREEN 
+    screen = pygame.display.set_mode(window_size, pygame.FULLSCREEN)
     
-    # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240,180))
-    
+    # load the provided background image
+    backgroundImage = pygame.image.load(config.getBackgroundImagePath()).convert()
+
+    # set the background image
+    screen.blit(backgroundImage, (0,0))
+
+    # update the displayed screen
+    pygame.display.update()
+
     # define a variable to control the main loop
     running = True
     
     # main loop
     while running:
+
         # event handling, gets all event from the eventqueue
         for event in pygame.event.get():
+
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
+
                 # change the value to False, to exit the main loop
                 running = False
-    
+
+        # use a sleep to prevent CPU hogging
+        time.sleep(1)    
+
     
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
