7eacf2aed669796ca6b46e3637a47e3cbd1577fc
OrthogonalHawk <orthogonalhawk@gmail.com>
2015-02-17 05:13:35 +0000

    Adding two buttons from the pygButton module.

--- TrainControlCenter.py
+++ TrainControlCenter.py
@@ -2,11 +2,16 @@
 #!/bin/env python
 
 import pygame
+import external.pygbutton as pygbutton
 import source.Configuration
 import sys
 import time
 
-window_size = (480, 272)
+# constants
+ORIGIN = (0, 0)
+WINDOW_WIDTH = 480
+WINDOW_HEIGHT = 272
+FPS = 10
 
 def printUsage():
     print "%s <configurationFilePath>" % (sys.argv[0])
@@ -19,42 +24,60 @@ def main():
         printUsage()
         sys.exit(-1)
 
-    # initialize the pygame module
+    # initialize the game module and clock
     pygame.init()
+    FPSCLOCK = pygame.time.Clock()
 
     # read the provided configuration file
     config = source.Configuration.Configuration(sys.argv[1])
 
     # create the display screen; set FULL SCREEN 
-    screen = pygame.display.set_mode(window_size, pygame.FULLSCREEN)
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.FULLSCREEN)
     
-    # load the provided background image
+    # load and set the provided background image
     backgroundImage = pygame.image.load(config.getBackgroundImagePath()).convert()
+    SCREEN.blit(backgroundImage, ORIGIN)
 
-    # set the background image
-    screen.blit(backgroundImage, (0,0))
+    # create buttons for user input
+    leftButton = pygbutton.PygButton(config.getLeftButtonRect(),
+                                     normal=config.getLeftButtonImage(),
+                                     down=config.getLeftButtonImage(),
+                                     highlight=config.getLeftButtonImage())  
 
-    # update the displayed screen
-    pygame.display.update()
+    middleButton = pygbutton.PygButton(config.getMiddleButtonRect(),
+                                       normal=config.getMiddleButtonImage(),
+                                       down=config.getMiddleButtonImage(),
+                                       highlight=config.getMiddleButtonImage())
 
-    # define a variable to control the main loop
-    running = True
-    
     # main loop
-    while running:
+    while True:
 
         # event handling, gets all event from the eventqueue
         for event in pygame.event.get():
 
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-                # change the value to False, to exit the main loop
-                running = False
+            # handle left button events
+            leftButtonEvents = leftButton.handleEvent(event)
+            if 'click' in leftButtonEvents:
+                pygame.mixer.music.load(config.getLeftFile())
+                pygame.mixer.music.play(0)
 
-        # use a sleep to prevent CPU hogging
-        time.sleep(1)    
+            middleButtonEvents = middleButton.handleEvent(event)
+            if 'click' in middleButtonEvents:
+                pygame.mixer.music.load(config.getMiddleFile())
+                pygame.mixer.music.play(0)
 
+        # add the button(s) to the display
+        leftButton.draw(SCREEN)
+        middleButton.draw(SCREEN)
+
+        # update the screen and sleep for a short period
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
     
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
--- source/Configuration.py
+++ source/Configuration.py
@@ -1,10 +1,19 @@
 
+import pygame
+
 class Configuration:
 
     optionNotFoundString = "ERROR - Unable to provide configuration option %s. No value found in file %s"
 
     # Expected options
     backgroundImageOption = "BackgroundImage"
+    leftButtonRectOption = "LeftButtonRect"
+    leftButtonImageOption = "LeftButtonImage"
+    leftButtonFileOption = "LeftButtonFile"
+ 
+    middleButtonRectOption = "MiddleButtonRect"
+    middleButtonImageOption = "MiddleButtonImage"
+    middleButtonFileOption = "MiddleButtonFile"
 
     def __init__(self, configFilePath):
         """Class constructor; parses the provided configuration file."""
@@ -15,7 +24,7 @@ class Configuration:
         if (optionName in self.configurationOptions.keys()):
             return self.configurationOptions[optionName]
         else:
-            print self.optionNofFoundString % (optionName, self.configFilePath)
+            print self.optionNotFoundString % (optionName, self.configFilePath)
             return ""
 
     def parseConfigurationFile(self, configFilePath):
@@ -46,4 +55,34 @@ class Configuration:
 
     def getBackgroundImagePath(self):
         return self._getConfigurationOption(self.backgroundImageOption)
+
+    def getRectOption(self, optionName):
+        rectString = self._getConfigurationOption(optionName)
+  
+        # split this on the semi-colon characters to form a list
+        rectParams = rectString.split(';')
+
+        rectList = []
+        for param in rectParams:
+            rectList.append(int(param))
+
+        return pygame.Rect(tuple(rectList))
+
+    def getLeftButtonRect(self):
+        return self.getRectOption(self.leftButtonRectOption)
+
+    def getLeftButtonImage(self):
+        return self._getConfigurationOption(self.leftButtonImageOption)
+
+    def getMiddleButtonRect(self):
+        return self.getRectOption(self.middleButtonRectOption)
+
+    def getLeftFile(self):
+        return self._getConfigurationOption(self.leftButtonFileOption)
+
+    def getMiddleButtonImage(self):
+        return self._getConfigurationOption(self.middleButtonImageOption)
+
+    def getMiddleFile(self):
+        return self._getConfigurationOption(self.middleButtonFileOption)
 
